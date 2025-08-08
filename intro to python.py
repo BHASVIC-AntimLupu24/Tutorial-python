14972c12ba7c98aebf11792c9c35563536c8f186
BHASVIC-AntimLupu24 <antim.lupu24@students.bhasvic.ac.uk>
2025-08-08 12:24:05 +0300

    2 hours in

--- intro to python.py
+++ intro to python.py
@@ -56,3 +56,73 @@
 #
 # def is_adult2(age):
 #     return True if age > 18 else return False
+#x = 'beau'
+#print('au' in x)
+#name = 'be\'au'
+#print(name)
+#name ='ja"mie'
+#print(name)
+# for double and single quote
+#name_2 = 'je"\'nifar'
+#print(name_2)
+#string = 'beau is cool'
+#print(string[1:7])
+#done = True
+#print(type(done) == bool) # checks if a value is boolean
+#if done:
+#    print('yes') # prints yes when done = true or when any variable is equal to true
+#else:
+#    print('no')
+#num_1 = 2+3j
+#num_2 = complex(2,3)
+#print(num_1.real)
+#print(num_1.imag)
+#print(num_2.real)
+#print(num_2.imag)
+#from enum import Enum
+#class State(Enum):
+#    INACTIVE = 0
+#    ACTIVE = 1
+#print(State.ACTIVE.value) # assigns a constant value
+#print(list(State)) # prints out the types of states
+#print(len(State)) # prints out how many states are there
+#age = input('What is your age? ')
+#print("your age is "+ age)
+# items = ['Rodger','Syd', 1, 2, True]
+# print('beau' in dogs) # checks if an item is in an list
+# print(dogs[1])
+# print(dogs.pop())
+# print(dogs)
+# dogs.insert(1,'Jean')
+# print(dogs)
+# items[1:1] = ['Test1', 'Test2']
+# print(items)
+
+# names = ['Jamie', 'Hubert','Amy','ben', 'Dean', 'Max', ]
+# namescopy = names[:]
+# names.sort(key=str.lower)
+# print(sorted(names, key=str.lower))
+# print(names)
+# print(namecopy)
+
+# names = ('Rodger', 'Syd')
+# names[0]
+# print(names[0])
+
+dog = {'name':'Rodger', 'age': 25, 'colour': 'green'}
+# dog['name'] = 'Syd'
+# print(dog['name'])
+# print(dog.get('name'))
+# print(dog.pop('name'))
+# print(dog)
+# print(dog.popitem())
+# print(dog)
+# print('colour' in dog)
+# print(list(dog.keys()))
+# print(list(dog.values()))
+# print(list(dog.items()))
+# dog['favourite food'] = 'Meat' # adding an item to a list
+# print(dog)
+# del dog['colour']
+# print(dog)
+dogCopy
